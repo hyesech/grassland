627473a4b9055dbb34351a883a04e77f6f252ee7
장혜승 <hyesech@gmail.com>
2022-11-14 15:21:51 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    console.log('Component', Component);
+    console.log('pageProps', pageProps);
+
+    return <Component {...pageProps} />;
 }
