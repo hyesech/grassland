ba800004e162886684930e32c78ed423ad696478
장혜승 <hyesech@gmail.com>
2022-11-16 14:06:45 +0900

    env 세팅, prod, dev 분기

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const nextEnv = require('next-env');
+
+const env = nextEnv();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    console.log('-------------DEVELOPMENT MODE-------------');
+  } else {
+    console.log('-------------PRODUCTION MODE-------------');
+  }
+  return env([defaultConfig, nextConfig]);
+};
